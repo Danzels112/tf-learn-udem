17054f5d2283c4a743a022798ae7e158f06b4f21
Danzels <dans@tvnet.lv>
2021-07-07 17:25:24 +0300

    for_each MAP approach

---
+++ terraform-resources/for-each/s3-bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "test_buckets" {
+  acl = "private"
+
+  for_each = {
+    "devotedone"  = "cryptoddds"
+    "stag" = "vaultddds"
+    "prod" = "housedddds"
+    "qa"   = "testitdddds"
+  }
+
+  bucket = "${each.key}-${each.value}"
+
+  tags = {
+    "env"     = each.key
+    "key-val" = each.value
+  }
+
+}
