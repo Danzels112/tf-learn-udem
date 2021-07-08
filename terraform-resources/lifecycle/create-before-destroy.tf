1f3f175099d06fac59846b1133b9edfec1b38f00
Danzels <dans@tvnet.lv>
2021-07-08 14:41:31 +0300

    example of prevent_destroy from lifecylce{}

--- terraform-resources/lifecycle/create-before-destroy.tf
+++ terraform-resources/lifecycle/create-before-destroy.tf
@@ -1,3 +1,8 @@
+/*
+By default terraform would destroy the resource before creating it. Using lifecycle argument with create_before_destroy you can
+change the default behaviour. By this new resource is provisioned before old one is created. See the example with aws ec2 inst-
+ance below.
+ */
 resource "aws_instance" "test-vm" {
   ami               = "ami-089b5384aac360007"
 #availability_zone = "eu-central-1a"
@@ -10,7 +15,8 @@ resource "aws_instance" "test-vm" {
   }
 
   lifecycle {
-    create_before_destroy = true
+    # create_before_destroy = true # Used in 1st example of lifecycle block.create_before_destroy 
+    # prevent_destroy = true
   }
 
 }
