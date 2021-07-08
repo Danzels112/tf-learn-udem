5514fcd4da2a85a2e90439571ebef3cc4d2b090e
Danzels <dans@tvnet.lv>
2021-07-08 11:20:36 +0300

    Added comments for iam-users.tf explaining the use of

--- terraform-resources/for-each/iam-users.tf
+++ terraform-resources/for-each/iam-users.tf
@@ -1,3 +1,6 @@
+
+/* Another approach to uses for_each is to provide values in a list and specify parameter as `each.key` */
+
 resource "aws_iam_user" "user_list" {
 
     for_each = toset(["Jameson", "Andrews", "Caputre", "Fabulos"])
