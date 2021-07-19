5674ee6ed54fdd9b206063ef23be3d00353a698d
Danzels <dans@tvnet.lv>
2021-07-19 17:34:14 +0300

    Datasources example: vpc/ami

---
+++ Datasources/ec2.tf
@@ -0,0 +1,21 @@
+
+resource "aws_instance" "test-ins" {
+  ami           = data.aws_ami.amxlinux.id
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
+  user_data     = <<-EOF
+    #!/bin/bash
+    sudo yum update -yum
+    sudo yum install -y httpd
+    sudo service httpd start
+    sudo systemctl enable httpd
+    echo "<h1> Welcome to the Hell! Infra created terrafrom in eu-central-1<h1>" > /var/www/html/index.html
+    EOF
+
+  tags = {
+    "Name" = "my-instance"
+  }
+
+  vpc_security_group_ids = [aws_security_group.custom_fw_rules.id]
+
+}
---
+++ Datasources/variables.tf
@@ -0,0 +1,22 @@
+# variable "ec2_ami_id" {
+#   description = "Amazon Linux ami ID"
+#   type        = string
+
+# }
+variable "all_cidr" {
+  description = "CIDR block for all internet"
+  type        = string
+  default = "0.0.0.0/0"
+}
+
+variable "aws_region" {
+  description = "Region where resource will be deployed"
+  type        = string
+  default = "eu-central-1"
+}
+
+variable "ec2_instance_type" {
+  description = "ec2 instance type"
+  type        = string
+  default = "t2.micro"
+}
